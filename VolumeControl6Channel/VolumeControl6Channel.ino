12033a67e32a3842462c1ac0733db7ae4828a1e4
Michal Mikloš <walkerr47@gmail.com>
2023-08-22 09:04:59 +0200

    volume control - devel

--- VolumeControl6Channel/VolumeControl6Channel.ino
+++ VolumeControl6Channel/VolumeControl6Channel.ino
@@ -6,7 +6,7 @@
 #include <EEPROM.h>
 #include <TM1637Display.h>
 
-#define DEBUG 0
+#define DEBUG 1
 #define RECV_PIN 7
 #define HOLD 0xFFFFFFFF
 #define DELAY 250
@@ -18,14 +18,14 @@ unsigned long last_code;
 unsigned long delay_t = DELAY;
 IRrecv irrecv(RECV_PIN);
 PT2258 pt2258(0x88);
-Value<int> volume(0, 0, 79, true);
+Value<int> volume(0, -79, 0, true);
 int volume_eeprom = 0;
 Timer save_timer(10000, false, false);
 TM1637Display display(CLK, DIO);
 
 void setup() {
   Serial.begin(115200);  // start the Serial communication
-
+  Serial.println("Starting...");
   Wire.begin();  // start the I2C bus
   //Wire.setClock(100000);  // the PT2258 is specified to work with a bus clock of 100kHz
 
@@ -35,7 +35,7 @@ void setup() {
   }
 
   irrecv.enableIRIn();
-  pt2258.volumeAll(0);  // at the beginning the volume is by default at 100%. Set the desired volume at startup before unmuting next
+  pt2258.volumeAll(-79);  // at the beginning the volume is by default at 100%. Set the desired volume at startup before unmuting next
   pt2258.mute(false);   // the mute is active when the device powers up. Unmute it to ear the sound
 
   EEPROM.get(0, volume_eeprom);
@@ -43,6 +43,9 @@ void setup() {
     volume.set(volume_eeprom);
   }
   display.setBrightness(0x0f);
+  display.clear();
+  display.showNumberDec(volume.get(), false, 3, 1);
+  Serial.println("Started!");
 }
 
 void loop() {
@@ -52,16 +55,16 @@ void loop() {
 
     if (results.value == HOLD) {
       results.value = last_code;
-      delay_t = 150;
+      delay_t = 100;
     } else {
       last_code = results.value;
       delay_t = DELAY;
     }
 
-    if (results.value == 0xFFA857) {  // +
+    if (results.value == 0xC03F20DF) {  // +
       volume.inc();
       if (DEBUG) Serial.println("Stisknuto VOL+");
-    } else if (results.value == 0xFFE01F) {  // -
+    } else if (results.value == 0xC03F10EF) {  // -
       volume.dec();
       if (DEBUG) Serial.println("Stisknuto VOL-");
     }  // else mute
@@ -70,9 +73,10 @@ void loop() {
   }
 
   if (volume.change()) {
-    pt2258.attenuationAll(volume.get());
-    pt2258.mute(volume.get() > 0 ? false : true);
-    display.showNumberDec(volume.get(), false, 2, 0);
+    pt2258.attenuationAll( volume.get() );
+    pt2258.mute(volume.get() > -79 ? false : true);
+    display.showNumberDec(volume.get(), false, 3, 1);
+    Serial.println(volume.get());
     save_timer.restart();
   }
 
